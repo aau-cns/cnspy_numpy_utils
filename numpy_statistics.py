08f92097a0afe12031d1abfa2498449400e99bc9
rojung <roland.jung@aau.at>
2020-10-16 11:45:06 +0200

    [TASK] EvaluationAnalyzer() uses numpy_statistics

--- numpy_statistics.py
+++ numpy_statistics.py
@@ -22,26 +22,38 @@
 import numpy as np
 
 
-def numpy_statistics(vNumpy):
-    metrics = {
-        'pairs': len(vNumpy),
-        'rmse': np.sqrt(np.dot(vNumpy, vNumpy) / len(vNumpy)),
-        'mean': np.mean(vNumpy),
-        'median': np.median(vNumpy),
-        'std': np.std(vNumpy),
-        'var': np.square(np.std(vNumpy)),
-        'min': np.min(vNumpy),
-        'max': np.max(vNumpy)
-    }
+def numpy_statistics(vNumpy, prefix=''):
+    if prefix:
+        metrics = {
+            prefix + '.pairs': len(vNumpy),
+            prefix + '.rmse': np.sqrt(np.dot(vNumpy, vNumpy) / len(vNumpy)),
+            prefix + '.mean': np.mean(vNumpy),
+            prefix + '.median': np.median(vNumpy),
+            prefix + '.std': np.std(vNumpy),
+            prefix + '.var': np.square(np.std(vNumpy)),
+            prefix + '.min': np.min(vNumpy),
+            prefix + '.max': np.max(vNumpy)
+        }
+    else:
+        metrics = {
+            'pairs': len(vNumpy),
+            'rmse': np.sqrt(np.dot(vNumpy, vNumpy) / len(vNumpy)),
+            'mean': np.mean(vNumpy),
+            'median': np.median(vNumpy),
+            'std': np.std(vNumpy),
+            'var': np.square(np.std(vNumpy)),
+            'min': np.min(vNumpy),
+            'max': np.max(vNumpy)
+        }
     return metrics
 
 
 def print_statistics(metrics, desc='error'):
-    print ("samples   %d" % (metrics['pairs']))
-    print ("%s.rmse   %f m" % (str(desc), metrics['rmse']))
-    print ("%s.mean   %f m" % (str(desc), metrics['mean']))
-    print ("%s.median %f m" % (str(desc), metrics['median']))
-    print ("%s.std    %f m" % (str(desc), metrics['std']))
-    print ("%s.var    %f m" % (str(desc), metrics['var']))
-    print ("%s.min    %f m" % (str(desc), metrics['min']))
-    print ("%s.max    %f m" % (str(desc), metrics['max']))
+    print("samples   %d" % (metrics['pairs']))
+    print("%s.rmse   %f m" % (str(desc), metrics['rmse']))
+    print("%s.mean   %f m" % (str(desc), metrics['mean']))
+    print("%s.median %f m" % (str(desc), metrics['median']))
+    print("%s.std    %f m" % (str(desc), metrics['std']))
+    print("%s.var    %f m" % (str(desc), metrics['var']))
+    print("%s.min    %f m" % (str(desc), metrics['min']))
+    print("%s.max    %f m" % (str(desc), metrics['max']))
