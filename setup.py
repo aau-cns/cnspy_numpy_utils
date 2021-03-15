ec06596545b9509b157428d9fc9a8a6596ed14bf
jungr-ait <roland.jung@aau.at>
2021-03-15 10:59:20 +0100

    [TASK] Setup.py modified

--- setup.py
+++ setup.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+from os import path
 
-setup(name='numpy_utils',
-      version='1.0',
-      description='Python Distribution Utilities',
-      author='Roland Jung',
-      author_email='roland.jung@aau.at',
-      url='https://gitlab.aau.at/aau-cns/py3_pkgs/numpy_utils/',
-      packages=['distutils', 'distutils.command', 'numpy'],
-     )
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# Get the release/version string
+with open(path.join(here, 'RELEASE'), encoding='utf-8') as f:
+    release = f.read()
+
+setup(
+    name='numpy_utils',
+    version=release,
+    author='Roland Jung',
+    author_email='roland.jung@aau.at',    
+    description='Some utility functions for numpy data types',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://gitlab.aau.at/aau-cns/py3_pkgs/numpy_utils/',
+    project_urls={
+        "Bug Tracker": "https://gitlab.aau.at/aau-cns/py3_pkgs/numpy_utils/issues",
+    },    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    
+    packages=find_packages(exclude=["test_*", "TODO*"]),
+    python_requires='>=3.6',
+    install_requires=['numpy'],
+)
